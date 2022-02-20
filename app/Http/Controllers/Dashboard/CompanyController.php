f0cb3730b882f03dd753391c24782f68f7d21a54
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-20 15:49:12 +0200

    new update curr

--- app/Http/Controllers/Dashboard/CompanyController.php
+++ app/Http/Controllers/Dashboard/CompanyController.php
@@ -84,7 +84,7 @@ class CompanyController extends Controller
                 $companiesnational = curl_exec($curl2);
 
                 $national = json_decode($companiesnational, true);
-                return $national['data'];
+                return $national['data']['childs'];
                 $compsave = new Company;
                 $allcompanyid = array();
                 foreach ($national['data'] as $company) {
