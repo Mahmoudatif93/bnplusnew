e8244195b05c03f71a43d9b4a7456f55dbbe9f85
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-21 06:42:20 +0200

    new update curr

--- app/Http/Controllers/Dashboard/currancyController.php
+++ app/Http/Controllers/Dashboard/currancyController.php
@@ -69,7 +69,7 @@ class currancyController extends Controller
         
         foreach(Cards::where('api',1)->get() as $cards ){
             $newprice['card_price']=$cards->card_price /$category->amount;
-            echo $newprice['card_price']; echo "<br>";
+           // echo $newprice['card_price']; echo "<br>";
           //  Cards::where('api',1)->update($newprice);
 
             $newprice2['card_price']=$cards->card_price *$request->amount;
