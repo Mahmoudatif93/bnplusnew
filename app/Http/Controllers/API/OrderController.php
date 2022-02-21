7c59ed12543010c6f0ddeba9b87fa3b4fcd2dc22
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-21 11:58:42 +0200

    new update curr

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -34,7 +34,7 @@ class OrderController extends Controller
         $cardscount = Cards::where(array('id' => $request->card_id,'card_price' => $request->card_price,'card_name'=>$request->card_name,'avaliable' => 0, 'purchase' => 0))->count();
 
         if ($cardscount > 0) {
-            $card = Cards::where(array('avaliable' => 0, 'purchase' => 0, 'card_price' => $request->card_price,'card_name'=>$request->card_name))->orderBy('id', 'desc')->first();
+            $card = Cards::where(array('id' => $request->card_id,'avaliable' => 0, 'purchase' => 0, 'card_price' => $request->card_price,'card_name'=>$request->card_name))->orderBy('id', 'desc')->first();
 
             $request_data['card_id'] = $card->id;
             $request_data['client_id'] = $request->client_id;
