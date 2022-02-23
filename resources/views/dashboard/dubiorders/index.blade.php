7d08af24ce5abfddc2ff5bfee6b21e24f6f5d6f3
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-24 00:14:07 +0200

    new update curr

--- resources/views/dashboard/dubiorders/index.blade.php
+++ resources/views/dashboard/dubiorders/index.blade.php
@@ -68,7 +68,7 @@
                                     <td>{{ $category['orderPaymentMethod'] }}</td>
 
                                     <td>
-                                    <a class="btn btn-primary btn-sm order-products" target="_blank"
+                                    <a class="btn btn-primary btn-sm" target="_blank"
                                                         href="{{ route('dashboard.dubiorders.products', $category['orderNumber']) }}">
                                                         
                                                 
