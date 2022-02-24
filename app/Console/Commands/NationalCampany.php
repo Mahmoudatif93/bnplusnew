1b01d19983450c5590d019836b4977ee3331a328
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-24 02:43:25 +0200

    new update curr

--- app/Console/Commands/NationalCampany.php
+++ app/Console/Commands/NationalCampany.php
@@ -42,9 +42,6 @@ class NationalCampany extends Command
      */
     public function handle()
     {
-
-
-        
         $allcardsid = array();
         $curl = curl_init();
 
@@ -59,10 +56,10 @@ class NationalCampany extends Command
             CURLOPT_CUSTOMREQUEST => "POST",
             CURLOPT_POSTFIELDS => array(
                 'deviceId' => 'cd63173e952e3076462733a26c71bbd0b236291db71656ec65ee1552478402ef',
-                    'email' => 'info@bn-plus.ly',
-                    'password' => 'db7d8028631f3351731cf7ca0302651d',
-                    'securityCode' => 'cd63173e952e3076462733a26c71bbd077d972e07e1d416cb9ab7f87bfc0c014',
-                    'langId' => '1'
+                'email' => 'info@bn-plus.ly',
+                'password' => 'db7d8028631f3351731cf7ca0302651d',
+                'securityCode' => 'cd63173e952e3076462733a26c71bbd077d972e07e1d416cb9ab7f87bfc0c014',
+                'langId' => '1'
             ),
 
         ));
@@ -102,16 +99,16 @@ class NationalCampany extends Command
                 $companiesnational = curl_exec($curl2);
 
                 $national = json_decode($companiesnational, true);
-                //  return $national['data'];
+                // return $national['data'];
                 $compsave = new Company;
                 $allcompanyid = array();
                 foreach ($national['data'] as $companys) {
 
                     foreach ($companys['childs'] as $company) {
 
-                       
+
                         if (count(Company::where('id', $company['id'])->get()) < 1) {
-                           // dd($companiesnational);
+                            // dd($companiesnational);
                             $request_data['id'] = $company['id'];
                             $request_data['company_image'] = $company['amazonImage'];
                             $request_data['name'] = $company['categoryName'];
@@ -120,10 +117,10 @@ class NationalCampany extends Command
 
 
                             Company::create($request_data);
-                        //   $compsave->save();
+                            //   $compsave->save();
 
 
-                          //  array_push($allcompanyid, $company['id']);
+                            //  array_push($allcompanyid, $company['id']);
                         }
 
                         // return($companiesnational);
@@ -159,10 +156,10 @@ class NationalCampany extends Command
                         $cardsnational = curl_exec($curl3);
 
                         $allcards = json_decode($cardsnational, true);
-//return $cardsnational ;
+                        //return $cardsnational ;
 
                         $cardsave = new Cards;
-                       
+
                         if (count($allcards) > 0) {
                             $curr =  Currency::first();
                             if (isset($allcards['data'])) {
@@ -170,20 +167,20 @@ class NationalCampany extends Command
                                     //    Cards::where('id', $card['productId'])->delete();
 
 
-                                    
-                                    if (count(Cards::where(array('productId'=>$card['productId'],'purchase'=>0))->get()) > 0) {
-                                       
+
+                                    if (count(Cards::where(array('productId' => $card['productId'], 'purchase' => 0))->get()) > 0) {
+
                                         foreach (Cards::where('productId', $card['productId'])->get() as $cardprice) {
                                             if ($cardprice->card_price != $card['sellPrice'] * $curr->amount) {
                                                 $oldprice['card_price'] = $card['sellPrice'] * $curr->amount;
                                                 Cards::where('id', $card['productId'])->update($oldprice);
                                             }
 
-                                           // return $card['productOptionalFields'] ;
-                                          /*  foreach( $card['productOptionalFields']  as $Cardid){
+                                            // return $card['productOptionalFields'] ;
+                                            /*  foreach( $card['productOptionalFields']  as $Cardid){
                                                 $cardsave->id=$Cardid['id'];
                                             }*/
-                                            
+
                                             $cardsave1['productId'] =  $card['productId'];
                                             $cardsave1['company_id'] = $card['categoryId'];
                                             $cardsave1['card_name'] = $card['productName'];
@@ -198,238 +195,42 @@ class NationalCampany extends Command
                                             $cardsave1['nationalcompany'] = 'national';
                                             $cardsave1['api'] = 1;
 
-                                    
 
-                                        Cards::create($cardsave1);
+
+                                            Cards::create($cardsave1);
                                         }
                                         array_push($allcardsid, $card['productId']);
 
                                         //  print_r( $oldprice);
                                     } else {
-                                    
-                                      
-                                            dd($company['id']);
-                                               
-                                                $cardsave2['productId'] =  $card['productId'];
-                                                $cardsave2['company_id'] = $company['id'];
-                                                $cardsave2['card_name'] = $card['productName'];
-                                                if ($card['productCurrency'] == "SAR") {
-                                                    $cardsave2['card_code'] = $card['sellPrice'] * $curr->amount;
-                                                } else {
-                                                    $cardsave2['card_code']  = $card['sellPrice'];
-                                                }
+                                      //  dd($company['id']);
 
-                                                $cardsave2['card_code'] = $card['productName'];
-                                                $cardsave2['card_image'] = $card['productImage'];
-                                                $cardsave2['nationalcompany'] = 'national';
-                                                $cardsave2['api'] = 1;
 
-                                                Cards::create($cardsave2);
+                                      if (count(Company::where('id',$company['id'])->get()) > 0) {
+                                        $cardsave2['productId'] =  $card['productId'];
+                                        $cardsave2['company_id'] = $company['id'];
+                                        $cardsave2['card_name'] = $card['productName'];
+                                        if ($card['productCurrency'] == "SAR") {
+                                            $cardsave2['card_code'] = $card['sellPrice'] * $curr->amount;
+                                        } else {
+                                            $cardsave2['card_code']  = $card['sellPrice'];
+                                        }
 
-                                            
-                                            
-                                        
+                                        $cardsave2['card_code'] = $card['productName'];
+                                        $cardsave2['card_image'] = $card['productImage'];
+                                        $cardsave2['nationalcompany'] = 'national';
+                                        $cardsave2['api'] = 1;
+
+                                        Cards::create($cardsave2);
+                                    }
                                     }
                                 }
                             }
                         }
-                         
                     }
                 }
             }
         }
-
-
-
-        ///////////////////currancy 
-
-
-
-
-        /////////////dubi national api
-   /*     $allcardsid = array();
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://taxes.like4app.com/online/check_balance/",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => array(
-                'deviceId' => 'cd63173e952e3076462733a26c71bbd0b236291db71656ec65ee1552478402ef',
-                    'email' => 'info@bn-plus.ly',
-                    'password' => 'db7d8028631f3351731cf7ca0302651d',
-                    'securityCode' => 'cd63173e952e3076462733a26c71bbd077d972e07e1d416cb9ab7f87bfc0c014',
-                    'langId' => '1'
-            ),
-
-        ));
-
-        $balancenational = curl_exec($curl);
-
-        if (isset($balancenational) && !empty($balancenational) && $balancenational != 'error code: 1020') {
-
-
-            $json = json_decode($balancenational, true);
-            //  return $json['balance'];
-
-
-            if ($json['balance'] > 0) {
-
-                $curl2 = curl_init();
-
-                curl_setopt_array($curl2, array(
-                    CURLOPT_URL => "https://taxes.like4app.com/online/categories",
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_ENCODING => "",
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 0,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => "POST",
-                    CURLOPT_POSTFIELDS => array(
-                        'deviceId' => 'cd63173e952e3076462733a26c71bbd0b236291db71656ec65ee1552478402ef',
-                        'email' => 'info@bn-plus.ly',
-                        'password' => 'db7d8028631f3351731cf7ca0302651d',
-                        'securityCode' => 'cd63173e952e3076462733a26c71bbd077d972e07e1d416cb9ab7f87bfc0c014',
-                        'langId' => '1'
-                    ),
-
-                ));
-
-                $companiesnational = curl_exec($curl2);
-
-                $national = json_decode($companiesnational, true);
-                //  return $national['data'];
-                $compsave = new Company;
-                $allcompanyid = array();
-                foreach ($national['data'] as $companys) {
-
-                    foreach ($companys['childs'] as $company) {
-                        if (count(Company::where('id', $company['id'])->get()) == 0) {
-                            // dd($companiesnational);
-                            $compsave->id = $company['id'];
-                            $compsave->company_image = $company['amazonImage'];
-                            $compsave->name = $company['categoryName'];
-                            $compsave->kind = 'national';
-                            $compsave->api = 1;
-
-                            $compsave->save();
-
-
-                            array_push($allcompanyid, $company['id']);
-                        }
-
-                        // return($companiesnational);
-                        //  return count($allcompanyid);
-
-
-
-                        /////////////////cards 
-
-                        $curl3 = curl_init();
-
-                        curl_setopt_array($curl3, array(
-                            CURLOPT_URL => "https://taxes.like4app.com/online/products",
-                            CURLOPT_RETURNTRANSFER => true,
-                            CURLOPT_ENCODING => "",
-                            CURLOPT_MAXREDIRS => 10,
-                            CURLOPT_TIMEOUT => 0,
-                            CURLOPT_FOLLOWLOCATION => true,
-                            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                            CURLOPT_CUSTOMREQUEST => "POST",
-                            CURLOPT_POSTFIELDS => array(
-                                'deviceId' => 'cd63173e952e3076462733a26c71bbd0b236291db71656ec65ee1552478402ef',
-                                'email' => 'info@bn-plus.ly',
-                                'password' => 'db7d8028631f3351731cf7ca0302651d',
-                                'securityCode' => 'cd63173e952e3076462733a26c71bbd077d972e07e1d416cb9ab7f87bfc0c014',
-                                'langId' => '1',
-                                'categoryId' => $company['id']
-                                // 'ids[]' => $company['id']
-                            ),
-
-                        ));
-
-                        $cardsnational = curl_exec($curl3);
-
-                        $allcards = json_decode($cardsnational, true);
-//return $allcards ;
-
-                        $cardsave = new Cards;
-                       
-                        if (count($allcards) > 0) {
-                            $curr =  Currency::first();
-                            if (isset($allcards['data'])) {
-                                foreach ($allcards['data'] as $card) {
-                                    //    Cards::where('id', $card['productId'])->delete();
-
-
-                                    
-                                    if (count(Cards::where(array('productId'=>$card['productId'],'purchase'=>0))->get()) > 0) {
-                                       
-                                        foreach (Cards::where('productId', $card['productId'])->get() as $cardprice) {
-                                            if ($cardprice->card_price != $card['sellPrice'] * $curr->amount) {
-                                                $oldprice['card_price'] = $card['sellPrice'] * $curr->amount;
-                                                Cards::where('id', $card['productId'])->update($oldprice);
-                                            }
-
-                                            $cardsave->productId =  $card['productId'];
-                                            $cardsave->company_id = $card['categoryId'];
-                                            $cardsave->card_name = $card['productName'];
-                                            if ($card['productCurrency'] == "SAR") {
-                                                $cardsave->card_price = $card['sellPrice'] * $curr->amount;
-                                            } else {
-                                                $cardsave->card_price = $card['sellPrice'];
-                                            }
-
-                                            $cardsave->card_code = $card['productName'];
-                                            $cardsave->card_image = $card['productImage'];
-                                            $cardsave->nationalcompany = 'national';
-                                            $cardsave->api = 1;
-
-                                        $cardsave->save();
-
-
-                                        }
-                                      //  array_push($allcardsid, $card['productId']);
-
-                                        //  print_r( $oldprice);
-                                    } else {
-                                        if (count(Cards::where(array('productId'=>$card['productId'],'purchase'=>0))->get()) == 0) {
-                                          //  array_push($allcardsid, $card['productId']);
-                                            if (count(Company::where('id', $card['categoryId'])->get()) != 0) {
-                                               // array_push($allcardsid, $card['productId']);
-                                                $cardsave->productId =  $card['productId'];
-                                                $cardsave->company_id = $card['categoryId'];
-                                                $cardsave->card_name = $card['productName'];
-                                                if ($card['productCurrency'] == "SAR") {
-                                                    $cardsave->card_price = $card['sellPrice'] * $curr->amount;
-                                                } else {
-                                                    $cardsave->card_price = $card['sellPrice'];
-                                                }
-
-                                                $cardsave->card_code = $card['productName'];
-                                                $cardsave->card_image = $card['productImage'];
-                                                $cardsave->nationalcompany = 'national';
-                                                $cardsave->api = 1;
-
-                                            $cardsave->save();
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                         
-                    }
-                }
-            }
-        }
-*/
 
         $this->info('National Cummand Run successfully!.');
     }
