8eaafe06b1fb3a751991dd50f8732b72fd022559
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-25 23:52:37 +0200

    new update curr

--- resources/views/dashboard/dubiorders/index.blade.php
+++ resources/views/dashboard/dubiorders/index.blade.php
@@ -2,46 +2,50 @@
 
 @section('content')
 
-    <div class="content-wrapper">
+@if($dubiorders !='error')
 
-        <section class="content-header">
 
-            <h1>@lang('site.dubiorders')</h1>
 
-            <ol class="breadcrumb">
-                <li><a href="{{ route('dashboard.index') }}"><i class="fa fa-dashboard"></i> @lang('site.dashboard')</a></li>
-                <li class="active">@lang('site.dubiorders') {{count(  $dubiorders)}}</li>
-            </ol>
-        </section>
+<div class="content-wrapper">
 
-        <section class="content">
+    <section class="content-header">
 
-            <div class="box box-primary">
+        <h1>@lang('site.dubiorders')</h1>
 
-                <div class="box-header with-border">
+        <ol class="breadcrumb">
+            <li><a href="{{ route('dashboard.index') }}"><i class="fa fa-dashboard"></i> @lang('site.dashboard')</a></li>
+            <li class="active">@lang('site.dubiorders') {{count( $dubiorders)}}</li>
+        </ol>
+    </section>
 
-                    <h3 class="box-title" style="margin-bottom: 15px">@lang('site.dubiorders') (<small style="color: red;font-weight:bold"> {{count(  $dubiorders)}}</small> 
-                
-                    @lang('site.orders')  )
+    <section class="content">
+
+        <div class="box box-primary">
+
+            <div class="box-header with-border">
+
+                <h3 class="box-title" style="margin-bottom: 15px">@lang('site.dubiorders') (<small style="color: red;font-weight:bold"> {{count( $dubiorders)}}</small>
+
+                    @lang('site.orders') )
                 </h3>
 
-                 
 
-<button class="btn btn-block btn-primary print-btn"><i class="fa fa-print"></i> @lang('site.print')</button>
-               
-                   
-                </div><!-- end of box header -->
-                <div id="print-area">
+
+                <button class="btn btn-block btn-primary print-btn"><i class="fa fa-print"></i> @lang('site.print')</button>
+
+
+            </div><!-- end of box header -->
+            <div id="print-area">
                 <div class="box-body" id="frame">
 
                     @if (!empty($dubiorders))
 
-                        <table id="example" class="table table-hover">
+                    <table id="example" class="table table-hover">
 
-                            <thead>
+                        <thead>
                             <tr>
                                 <th>#</th>
-                               
+
                                 <th>@lang('site.orderNumber')</th>
                                 <th>@lang('site.orderFinalTotal')</th>
                                 <th>@lang('site.currencySymbol')</th>
@@ -50,62 +54,110 @@
                                 <th>@lang('site.orderPaymentMethod')</th>
                                 <th>@lang('site.action')</th>
                             </tr>
-                            </thead>
+                        </thead>
 
-                            <tbody>
+                        <tbody>
                             @foreach ($dubiorders as $index=>$category)
-                                <tr>
-                                    <td>{{ $index + 1 }}</td>
-                                    
-                                    <td>
-                                        
-                                 
-                                    {{ $category['orderNumber'] }}</td>
-                                    <td>{{ $category['orderFinalTotal'] }}</td>
-                                    <td>{{ $category['currencySymbol'] }}</td>
-                                    <td>{{ $category['orderCreateDate'] }}</td>
-                                    <td>{{ $category['orderCurrentStatus'] }}</td>
-                                    <td>{{ $category['orderPaymentMethod'] }}</td>
+                            <tr>
+                                <td>{{ $index + 1 }}</td>
 
-                                    <td>
-                                    <a class="btn btn-primary btn-sm" target="_blank"
-                                                        href="{{ route('dashboard.dubiorders.products', $category['orderNumber']) }}">
-                                                        
-                                                
-                                                    <i class="fa fa-list"></i>
-                                                    @lang('site.show')
-</a>
+                                <td>
 
 
-                                    </td>
-                                 
+                                    {{ $category['orderNumber'] }}
+                                </td>
+                                <td>{{ $category['orderFinalTotal'] }}</td>
+                                <td>{{ $category['currencySymbol'] }}</td>
+                                <td>{{ $category['orderCreateDate'] }}</td>
+                                <td>{{ $category['orderCurrentStatus'] }}</td>
+                                <td>{{ $category['orderPaymentMethod'] }}</td>
+
+                                <td>
+                                    <a class="btn btn-primary btn-sm" target="_blank" href="{{ route('dashboard.dubiorders.products', $category['orderNumber']) }}">
+
+
+                                        <i class="fa fa-list"></i>
+                                        @lang('site.show')
+                                    </a>
+
+
+                                </td>
 
 
 
-                                      
-                                </tr>
+
+
+                            </tr>
 
                             @endforeach
-                            </tbody>
+                        </tbody>
 
-                        </table><!-- end of table -->
+                    </table><!-- end of table -->
 
-                       
+
 
                     @else
 
-                        <h2>@lang('site.no_data_found')</h2>
+                    <h2>@lang('site.no_data_found')</h2>
 
                     @endif
 
                 </div><!-- end of box body -->
-</div>
+            </div>
 
-            </div><!-- end of box -->
+        </div><!-- end of box -->
 
-        </section><!-- end of content -->
+    </section><!-- end of content -->
 
-    </div><!-- end of content wrapper -->
+</div><!-- end of content wrapper -->
+@else
 
+
+<div class="content-wrapper">
+
+    <section class="content-header">
+
+        <h1>@lang('site.dubiorders')</h1>
+
+        <ol class="breadcrumb">
+            <li><a href="{{ route('dashboard.index') }}"><i class="fa fa-dashboard"></i> @lang('site.dashboard')</a></li>
+            <li class="active">@lang('site.dubiorders')</li>
+        </ol>
+    </section>
+
+    <section class="content">
+
+        <div class="box box-primary">
+
+            <div class="box-header with-border">
+
+                <h3 class="box-title" style="margin-bottom: 15px">@lang('site.dubiorders') (<small style="color: red;font-weight:bold"> </small>
+
+                    @lang('site.orders') )
+                </h3>
+
+
+
+                <button class="btn btn-block btn-primary print-btn"><i class="fa fa-print"></i> @lang('site.print')</button>
+
+
+            </div><!-- end of box header -->
+            <div id="print-area">
+                <div class="box-body" id="frame">
+
+                   
+
+                </div><!-- end of box body -->
+            </div>
+
+        </div><!-- end of box -->
+
+    </section><!-- end of content -->
+
+</div><!-- end of content wrapper -->
+
+
+
+@endif
 
 @endsection
