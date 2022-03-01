d8de1f53889b96806f43817c50abf8107c87435e
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-03-01 19:52:55 +0200

    new update curr

---
+++ app/Http/Controllers/Dashboard/DubaiOffController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DubaiOffController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        
+        $Companies = Company::where(array('enable'=>0,'api'=>1))->when($request->search, function ($q) use ($request) {
+
+            return $q->where('name','like', '%' .  $request->search . '%')
+                ->orWhere('kind', 'like', '%' . $request->search . '%');
+        })->latest()->paginate(5);
+
+        return view('dashboard.dubioff.index', compact('Companies'));
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
