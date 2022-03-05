eca68889512dcda430612a2f4cdbf393d87caeef
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-03-05 10:50:09 +0200

    new update curr

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -28,8 +28,8 @@ class UserController extends Controller
      */
     public function index(Request $request)
     {
-        $users = User::whereRoleIs('admin')->where(function ($q) use ($request) {
-
+     //   $users = User::whereRoleIs('admin')->where(function ($q) use ($request) {
+            $users = User::whereRoleIs('adminssss')->where(function ($q) use ($request) {
             return $q->when($request->search, function ($query) use ($request) {
 
                 return $query->where('first_name', 'like', '%' . $request->search . '%')
