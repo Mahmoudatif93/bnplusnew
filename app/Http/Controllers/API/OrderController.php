6cc75e7760bf4af5e613e0e21f50320ac0206cd6
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-03-06 20:43:15 +0200

    new update curr

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -36,10 +36,10 @@ class OrderController extends Controller
     {
 
 
-        $cardscount = Cards::where(array('id' => $request->card_id, 'avaliable' => 0, 'purchase' => 0,'enable'=>0))->count();
+        $cardscount = Cards::where(array('id' => $request->card_id, 'avaliable' => 0, 'purchase' => 0, 'enable' => 0))->count();
 
         if ($cardscount > 0) {
-            $card = Cards::where(array('id' => $request->card_id, 'avaliable' => 0, 'purchase' => 0,'enable'=>0))->orderBy('id', 'desc')->first();
+            $card = Cards::where(array('id' => $request->card_id, 'avaliable' => 0, 'purchase' => 0, 'enable' => 0))->orderBy('id', 'desc')->first();
 
             if ($card->api == 1) {
 
@@ -95,117 +95,127 @@ class OrderController extends Controller
         $id = $request->order_id;
         $order = Order::find($id);
         if (!empty($order)) {
-            $order->transaction_id = $request->transaction_id;
-            $order->paid = 'true';
-            $order->paymenttype = "معاملات";
-
-            $allcompanyid = array();
-            $curl = curl_init();
-
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => "https://taxes.like4app.com/online/check_balance/",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "POST",
-                CURLOPT_POSTFIELDS => array(
-                    'deviceId' => 'cd63173e952e3076462733a26c71bbd0b236291db71656ec65ee1552478402ef',
-                    'email' => 'info@bn-plus.ly',
-                    'password' => 'db7d8028631f3351731cf7ca0302651d',
-                    'securityCode' => 'cd63173e952e3076462733a26c71bbd077d972e07e1d416cb9ab7f87bfc0c014',
-                    'langId' => '1'
-                ),
-
-            ));
-
-            $balancenational = curl_exec($curl);
-
-            if (isset($balancenational) && !empty($balancenational) && $balancenational != 'error code: 1020') {
-
-                ////////////dubai api///////////////
 
 
 
-                $dubiapi =  Cards::where('id', $order->card_id)->first();
-                $clientdata =  Client::where('id', $order->client_id)->first();
-                if ($dubiapi->api == 1) {
-                    $client =  Client::where('id', $order->client_id)->first();
-                    $curl = curl_init();
-                    $refrenceid = "Merchant_" . rand();
-                    curl_setopt_array($curl, array(
-                        CURLOPT_URL => "https://taxes.like4app.com/online/create_order",
-                        CURLOPT_RETURNTRANSFER => true,
-                        CURLOPT_ENCODING => "",
-                        CURLOPT_MAXREDIRS => 10,
-                        CURLOPT_TIMEOUT => 0,
-                        CURLOPT_FOLLOWLOCATION => true,
-                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                        CURLOPT_CUSTOMREQUEST => "POST",
-                        CURLOPT_POSTFIELDS => array(
-                            'deviceId' => 'cd63173e952e3076462733a26c71bbd0b236291db71656ec65ee1552478402ef',
-                            'email' => 'info@bn-plus.ly',
-                            'password' => 'db7d8028631f3351731cf7ca0302651d',
-                            'securityCode' => 'cd63173e952e3076462733a26c71bbd077d972e07e1d416cb9ab7f87bfc0c014',
-                            'langId' => '1',
-                            'productId' => $order->card_id,
-                            'referenceId' => $refrenceid,
-                            'time' => time(),
-                            'hash' => $this->generateHash($clientdata->phone, $clientdata->email),
-
-                        ),
-
-                    ));
-
-                    $createorder = curl_exec($curl);
-
-                    $json = json_decode($createorder, true);
-                    //  return $json['serials'];
-
-                    foreach ($json['serials'] as $row) {
-                        //  return $row['serialCode'];
-                        $updatecardprice['card_code'] =  $this->decryptSerial($row['serialCode']);
-                        Cards::where('id', $order->card_id)->update($updatecardprice);
-                        $this->sendResetEmail($client->email, $this->decryptSerial($row['serialCode']), 'Your BNplus Code');
-                    }
-
-
-                    curl_close($curl);
-                }
-
-
-
-
-
-
-                /////////////
-                if ($order->update()) {
-                    $updatecard['purchase'] = 1;
-                    $updatecard['avaliable'] = 1;
-                    Cards::where('id', $order->card_id)->update($updatecard);
-
-                    $cardemail =  Cards::where('id', $order->card_id)->first();
-                    $client =  Client::where('id', $order->client_id)->first();
-                    if ($dubiapi->api == 0) {
-                        $this->sendResetEmail($client->email,  $cardemail->card_code, 'Your BNplus Code');
-                    }
-
-                    return response()->json(['status' => 'success']);
-                } else {
-                    return response()->json(['status' => 'error']);
-                }
-            } else {
-
+            $is_expired = $order->created_at->addMinutes(5);
+            if ($is_expired < \Carbon\Carbon::now()) {
 
                 return response()->json(['status' => 'error']);
+
+            } else {
+                $order->transaction_id = $request->transaction_id;
+                $order->paid = 'true';
+                $order->paymenttype = "معاملات";
+
+                $allcompanyid = array();
+                $curl = curl_init();
+
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => "https://taxes.like4app.com/online/check_balance/",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "POST",
+                    CURLOPT_POSTFIELDS => array(
+                        'deviceId' => 'cd63173e952e3076462733a26c71bbd0b236291db71656ec65ee1552478402ef',
+                        'email' => 'info@bn-plus.ly',
+                        'password' => 'db7d8028631f3351731cf7ca0302651d',
+                        'securityCode' => 'cd63173e952e3076462733a26c71bbd077d972e07e1d416cb9ab7f87bfc0c014',
+                        'langId' => '1'
+                    ),
+
+                ));
+
+                $balancenational = curl_exec($curl);
+
+                if (isset($balancenational) && !empty($balancenational) && $balancenational != 'error code: 1020') {
+
+                    ////////////dubai api///////////////
+
+
+
+                    $dubiapi =  Cards::where('id', $order->card_id)->first();
+                    $clientdata =  Client::where('id', $order->client_id)->first();
+                    if ($dubiapi->api == 1) {
+                        $client =  Client::where('id', $order->client_id)->first();
+                        $curl = curl_init();
+                        $refrenceid = "Merchant_" . rand();
+                        curl_setopt_array($curl, array(
+                            CURLOPT_URL => "https://taxes.like4app.com/online/create_order",
+                            CURLOPT_RETURNTRANSFER => true,
+                            CURLOPT_ENCODING => "",
+                            CURLOPT_MAXREDIRS => 10,
+                            CURLOPT_TIMEOUT => 0,
+                            CURLOPT_FOLLOWLOCATION => true,
+                            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                            CURLOPT_CUSTOMREQUEST => "POST",
+                            CURLOPT_POSTFIELDS => array(
+                                'deviceId' => 'cd63173e952e3076462733a26c71bbd0b236291db71656ec65ee1552478402ef',
+                                'email' => 'info@bn-plus.ly',
+                                'password' => 'db7d8028631f3351731cf7ca0302651d',
+                                'securityCode' => 'cd63173e952e3076462733a26c71bbd077d972e07e1d416cb9ab7f87bfc0c014',
+                                'langId' => '1',
+                                'productId' => $order->card_id,
+                                'referenceId' => $refrenceid,
+                                'time' => time(),
+                                'hash' => $this->generateHash($clientdata->phone, $clientdata->email),
+
+                            ),
+
+                        ));
+
+                        $createorder = curl_exec($curl);
+
+                        $json = json_decode($createorder, true);
+                        //  return $json['serials'];
+
+                        foreach ($json['serials'] as $row) {
+                            //  return $row['serialCode'];
+                            $updatecardprice['card_code'] =  $this->decryptSerial($row['serialCode']);
+                            Cards::where('id', $order->card_id)->update($updatecardprice);
+                            $this->sendResetEmail($client->email, $this->decryptSerial($row['serialCode']), 'Your BNplus Code');
+                        }
+
+
+                        curl_close($curl);
+                    }
+
+
+
+
+
+
+                    /////////////
+                    if ($order->update()) {
+                        $updatecard['purchase'] = 1;
+                        $updatecard['avaliable'] = 1;
+                        Cards::where('id', $order->card_id)->update($updatecard);
+
+                        $cardemail =  Cards::where('id', $order->card_id)->first();
+                        $client =  Client::where('id', $order->client_id)->first();
+                        if ($dubiapi->api == 0) {
+                            $this->sendResetEmail($client->email,  $cardemail->card_code, 'Your BNplus Code');
+                        }
+
+                        return response()->json(['status' => 'success']);
+                    } else {
+                        return response()->json(['status' => 'error']);
+                    }
+                } else {
+
+
+                    return response()->json(['status' => 'error']);
+                }
             }
         } else {
             return response()->json(['status' => 'error']);
         }
     }
-/*
+    /*
     public function finalordernotdubai($request)
     {
 
