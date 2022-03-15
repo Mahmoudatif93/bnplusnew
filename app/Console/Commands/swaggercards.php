27372aca5730c6f1fe312d7a15e4b0336a0fcb3e
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-03-15 19:24:00 +0200

    new update curr

---
+++ app/Console/Commands/swaggercards.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Carbon\Carbon;
+use App\Cards;
+use App\Company;
+use Illuminate\Support\Facades\Http;
+use App\Currency;
+
+class swaggercards extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'swagger:cards';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        ini_set("prce.backtrack_limit","100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+    
+
+
+        $this->info('swagger Cummand Run successfully!.');
+    }
+}
