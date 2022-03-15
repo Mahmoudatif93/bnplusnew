c0d673399247541502f141c9102fe0da47013b4a
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-03-15 18:42:28 +0200

    new update curr

--- app/Http/Controllers/Dashboard/CompanyController.php
+++ app/Http/Controllers/Dashboard/CompanyController.php
@@ -69,7 +69,7 @@ if (!empty($swaggercompanies->json()['data'])) {
 foreach($swaggercompanies->json()['data'] as $rowcomp){
 
 
-    dd($rowcomp->type);
+    dd($rowcomp['type']);
 
 
     if($rowcomp->type=='Local'){
