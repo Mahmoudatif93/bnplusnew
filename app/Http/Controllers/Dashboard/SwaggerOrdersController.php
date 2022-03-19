d19848ecc868cdaafd8e9039badc2ed5387f1f12
Mahmoudatif93 <mahmoudatif22@gmail.com>
2022-03-19 17:29:06 +0200

    new update curr

--- app/Http/Controllers/Dashboard/SwaggerOrdersController.php
+++ app/Http/Controllers/Dashboard/SwaggerOrdersController.php
@@ -25,30 +25,14 @@ class SwaggerOrdersController extends Controller
     public function index()
     {
        
-       $curl = curl_init();
-        $refrenceid = "Merchant_" . rand();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://gateway-staging.anis.ly/api/consumers/v1/transactions?pinNumber=1988",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-         
-
-        ));
-
-        $dubiorder = curl_exec($curl);
-        curl_close($curl);
-        $dubiordersjson = json_decode($dubiorder, true);
+   
 
 
-
-        $uri = 'https://gateway-staging.anis.ly/api/consumers/v1/transactions?pinNumber=1988';
-        $response = Http::asForm()->withHeaders([])->get($uri);   
-dd( $dubiordersjson );
+        $swaggercompanies = Http::withHeaders([
+            'Content-Type' => 'application/x-www-form-urlencoded'
+        ])->get('https://gateway-staging.anis.ly/api/consumers/v1/transactions?pinNumber=1988');
+  
+dd( $swaggercompanies );
 
 
 
