2c0fa83fd831524ce0d17ea240bba39a5a6ade4f
Mahmoudatif93 <mahmoudatif22@gmail.com>
2022-03-20 22:50:18 +0200

    new update curr

--- app/Http/Controllers/Dashboard/CompanyController.php
+++ app/Http/Controllers/Dashboard/CompanyController.php
@@ -47,6 +47,7 @@ class CompanyController extends Controller
         $token_type=$response->json()['token_type'];
         $alltoken=$response->json()['token_type'] .' '.$response->json()['access_token'];
 
+        dd($response->json());
         $orderswal = Http::withHeaders([
            'Accept' => 'application/json',
            'Authorization' => $alltoken,
@@ -63,7 +64,7 @@ if($orderswal->json()['data'] >0){
 
 
 
-    $uri = 'https://identity-staging.anis.ly/connect/token ';
+    $uri = 'https://identity.anis.ly/connect/token ';
     $params = array(
         'grant_type' => 'user_credentials',
         'client_id' => 'bn-plus',
@@ -72,8 +73,7 @@ if($orderswal->json()['data'] >0){
         'email' => 'info@bn-plus.ly',
     );
     $response = Http::asForm()->withHeaders([])->post($uri, $params);   
-
-    dd($response->json());
+    
 $token=$response->json()['access_token'];
 $token_type=$response->json()['token_type'];
 $alltoken=$response->json()['token_type'] .' '.$response->json()['access_token'];
