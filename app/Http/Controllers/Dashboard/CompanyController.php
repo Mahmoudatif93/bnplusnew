f2a497f71fdda8db8d4bda76471cc568a677dbc5
Mahmoudatif93 <mahmoudatif22@gmail.com>
2022-03-21 22:52:23 +0200

    new update curr

--- app/Http/Controllers/Dashboard/CompanyController.php
+++ app/Http/Controllers/Dashboard/CompanyController.php
@@ -33,7 +33,7 @@ class CompanyController extends Controller
         /// $this->sendResetEmail('zayedmahdi@yahoo.com', 'SgiXggkL2L2080N8ab	', 'Your BNplus Code');
     $dubiapi =  Cards::where('id',10477)->first();
    
-        $id=8079;
+        $id=80079;
                      
             //$client =  Client::where('id', $order->client_id)->first();
          //   rand();
